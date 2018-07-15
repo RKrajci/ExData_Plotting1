d7269f641758b31f0ac7a9e9d852a9cd919cb06a
RKrajci <rchapman.g@gmail.com>
2018-07-14 22:48:36 -0500

    Add Comments to R Scripts

--- plot4.R
+++ plot4.R
@@ -1,9 +1,16 @@
+#load sqldf package
 library(sqldf)
+
+#import subsetted data from text file, close connection
 text<-"household_power_consumption.txt"
 file<-file(text)
 data<-sqldf("select * from file where Date in ('1/2/2007','2/2/2007')", file.format=list(header=TRUE, sep=";", na.strings="?"))
 close(file)
+
+#create formatted datetime column
 data$datetime<-as.POSIXct(paste(as.Date(data$Date,"%d/%m/%Y"),format(strptime(data$Time,"%H:%M:%S"),"%H:%M:%S")),format="%Y-%m-%d %H:%M:%S")
+
+#create plot and send to PNG file
 par(mfrow=c(2,2))
 png(filename="plot4.png",width=480,height=480)
 plot(data$datetime,data$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
